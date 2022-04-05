758e242744bd8fc29ad564ebf9db3e125d7b3e1e
sehwan <goodya1f@gmail.com>
2022-04-05 23:08:16 +0900

    lotto

---
+++ demos/views.py
@@ -0,0 +1,35 @@
+
+from django.shortcuts import render
+from django.http import HttpResponse
+import random
+
+
+def calculator(request):
+
+    # 1.데이터 확인
+    num1 = request.GET.get('num1')
+    num2 = request.GET.get('num2')
+    operators = request.GET.get('operators')
+
+    # 2. 계산
+    if operators == '+':
+        result = num1 + num2
+    elif operators == '-':
+        result = num1 - num2
+    elif operators == '*':
+        result = num1 * num2
+    elif operators == '/':
+        result = num1 / num2
+    else:
+        result = 0
+
+    # 3. 응답
+    return render(request, 'calculator.html', {'result': result})
+
+
+def lotto(request):
+    list = range(1, 45)
+    lotto = random.sample(list, 7)
+    # print(lotto)
+
+    return render(request, 'lotto.html', {'lotto': lotto})
